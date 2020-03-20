29a2de089516de1aad71e6e52ac16e5515c477f3
CosmoXDD <benjaminsalazarballadares@gmail.com>
2020-03-20 15:52:11 -0300

    Final Test

--- src/main.c
+++ src/main.c
@@ -2,14 +2,29 @@
 #include <web/web.h>
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
-    printf("Asking for the user's URL.....");
+    consoleInit(NULL);
 
+    printf("Asking for the user's URL.....");
     char *url = keyboard("Write your url here. Please include the \"https://\"", sizeof(url));
-    
     launchWeb(url);
 
+    while (appletMainLoop())
+    {
+        hidScanInput();
+        u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+
+        if (kDown & KEY_PLUS)
+            break;
+
+        consoleUpdate(NULL);
+    }
+
+    free(url);
+    consoleExit(NULL);
     return 0;
 }
